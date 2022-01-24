8b16b4391b43823126fa48b63c94cdb0738fbc66
harlelokesh <harlelokesh@gmail.com>
2022-01-24 16:07:23 +0530

    test maven

---
+++ src/test/java/study/TestMain.java
@@ -0,0 +1,13 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+public class TestMain {
+
+	@Test
+	public void testInputIsEven(){
+		assertTrue(Main.checkIfInputIsAnEvenNumber(142));
+		//Assertion
+
+	}
+}
